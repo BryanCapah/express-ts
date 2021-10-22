dc1c070f859a951d065be9c082879a0d83eceb76
brian capah <briancapah@192.168.1.28>
2021-10-22 11:57:03 +0700

    asd

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+import dotenv from 'dotenv'
 import routes from './src/routes'
-
+dotenv.config()
 const app: Application = express();
 const port = process.env.PORT;
 
-// Body parsing Middleware
+app.use(cors({ origin: process.env.HOST }));
 app.use(express.json());
 app.use(express.raw());
 app.use(express.urlencoded({ extended: true }));
